7b22bd0a2e613d14e19854d7afe46fa39a2952bb
Anusha <wagleanusha11@gmail.com>
2022-05-04 16:52:06 +0545

    first commit

---
+++ src/App.js
@@ -0,0 +1,23 @@
+import React, { useState } from "react";
+
+const App =() =>{
+  const [userInput, setUserInput] = useState("")
+  const [todos, setTodos] = useState([])
+
+  const addData = ()=>{
+    setTodos([...todos,userInput])
+  }
+  return<div>
+    <h1>Todo List</h1>
+    <input onChange={(event)=> setUserInput(event.target.value )} type="text"></input>
+    <button onClick={()=> addData()}>Add</button>
+    <ul>
+      {
+        todos.map((todo, index)=> <li key = {index}>{todo}</li>)
+      }
+    </ul>
+  </div>
+}
+
+
+export default App;
